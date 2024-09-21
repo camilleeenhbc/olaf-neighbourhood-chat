c210574f46d515975adb8c4c0fe2b36f6a568936
Joanne <joannesu2003@gmail.com>
2024-09-22 09:16:17 +0930

    Finalising client and message

--- client.py
+++ client.py
@@ -2,65 +2,68 @@ import json
 import base64
 import os
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes  #for AES
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes  # for AES
 from cryptography.hazmat.primitives import hashes
+
 
 class Client:
     def __init__(self):
-        self.counter=0
-        self.privateKey=rsa.generatePrivateKey(
-            public_exponent=65537,    
-            key_size=2048,   #modulus length
-            backend=default_backend()
+        self.counter = 0
+        self.privateKey = rsa.generatePrivateKey(
+            public_exponent=65537,
+            key_size=2048,  # modulus length
+            backend=default_backend(),
         )
         self.publicKey = self.privateKey.publicKey()
         self.fingerprint = self.generateFingerprint(self.publicKey)
-        
-        
-    #Perform AES in GCM mode
-    #Key length of 32 bytes (128 bits)
-    def encryptWithAES(self, key):
-        #IV should be 16 bytes (randomly generated)
-        iv=os.urandom(16)
-        
-        cipher = Cipher(algorithms.AES(key),modes.GCM(iv))
-        encryptor=cipher.encryptor()
-        self.encryptedContent=encryptor.update(self.content.encode())+encryptor.finalize()
-        #Encode key with base64
-        self.symmKeys.append(base64.b64encode(key).decode())
-    
-    #Sign the message using the RSA-PSS scheme
-    #Signature should be Base64 of data + counter
-    def signMessage(self, privateKey):
-        message=self.content+str(self.counter).encode()
-        signature=privateKey.sign(
-        message,padding.PSS(
-            mgf=padding.MGF1(hashes.SHA256()),
-            salt_length=padding.PSS.MAX_LENGTH
+
+    # Function to encrypt the AES key
+    def encrypt(self, receiverPublicKey, aesKey):
+        return receiverPublicKey.encrypt(
+            aesKey,
+            # Apply OAEP padding
+            padding.OAEP(
+                # SHA-256 digest/hash function used
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None,
             ),
-        hashes.SHA256()
+        )
+
+    #Decrypt the AES key
+    def decrypt(self, aesKey):
+        return self.private_key.decrypt(
+            aesKey,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+    # Sign the message using the RSA-PSS scheme
+    # Signature should be Base64 of data + counter
+    def signMessage(self, message):
+        signature = self.privateKey.sign(
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256(),
         )
         self.signature = base64.b64.encode(signature).decode()
-    
-    def verifySignature(self,publicKey):
+        return self.signature
+
+    def verifySignature(self, publicKey):
         try:
             publicKey.verify(
-                base64.b64decode(self.signature),(self.content + str(self.counter)).encode(),
+                base64.b64decode(self.signature),
+                (self.content + str(self.counter)).encode(),
                 padding.PSS(
-                    mgf=padding.MGF1(hashes.SHA256),
-                    salt_length=padding.PSS.MAX_LENGTH
+                    mgf=padding.MGF1(hashes.SHA256), salt_length=padding.PSS.MAX_LENGTH
                 ),
-                hashes.SHA256()
+                hashes.SHA256(),
             )
             return True
         except:
             return False
-            
-
-    def decrypt_with_aes(self, key: bytes):
-        #IV should be 16 bytes (randomly generated)
-        iv=os.urandom(16)
-        cipher = Cipher(algorithms.AES(key),modes.GCM(iv))
-        decryptor=cipher.decryptor()
-        self.content=decryptor.update(self.content.encode())+decryptor.finalize()
-        print(self.content)
--- message.py
+++ message.py
@@ -2,64 +2,59 @@ import json
 import base64
 import os
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes  # for AES
 from cryptography.hazmat.primitives import hashes
 
+
 class Message:
-    def __init__(self, content, messageType="chat",destinationServers=None):
+    def __init__(self, content, messageType="chat", destinationServers=None):
         # assign the attributes
         self.content = content
         self.encryptedContent = None
         self.participants = []
-        self.iv = None #base64 encoded AES initialisation vector
+        self.iv = None  # base64 encoded AES initialisation vector
         self.symmKeys = []
         self.signature = None
-        self.counter = 0    #the nonce
-        self.messageType=messageType
+        self.counter = 0  # the nonce
+        self.messageType = messageType
         self.destinationServers = destinationServers if destinationServers else []
-    
-    # #Function to encrypt message with receiver's public key with OAEP padding
-    # def encryptWithRSA(self, receiverPublicKey):
-    #     self.encryptedContent = receiverPublicKey.encrypt(
-    #         self.content.encode(), #convert string
-    #         #Apply OAEP padding
-    #         padding.OAEP(
-    #             #SHA-256 digest/hash function used
-    #             mgf=padding.MGF1(algorithm=hashes.SHA256()),
-    #             algorithm=hashes.SHA256(),
-    #             label=None
-    #         )
-    #     )
-        
-        
-    # def decrypt_with_rsa(self, privateKey):
-    #     self.encryptedContent = base64.b64decode(self.encryptedContent) 
-        
-    #     decrypted_content = privateKey.decrypt(
-    #         self.encryptedContent,
-    #         padding.OAEP(
-    #             mgf=padding.MGF1(algorithm=hashes.SHA256()),
-    #             algorithm=hashes.SHA256(),
-    #             label=None
-    #         )
-    #     )
-    #     self.content = decrypted_content.decode('utf-8') 
+
+    # Perform AES in GCM mode
+    # Key length of 32 bytes (128 bits)
+    def encryptWithAES(self, key):
+        # IV should be 16 bytes (randomly generated)
+        self.iv = os.urandom(16)
+
+        cipher = Cipher(algorithms.AES(key), modes.GCM(self.iv))
+        encryptor = cipher.encryptor()
+        self.encryptedContent = (
+            encryptor.update(self.content.encode()) + encryptor.finalize()
+        )
+        # Encode key with base64
+        self.symmKeys.append(base64.b64encode(key).decode())
+
+    def decrypt_with_aes(self, key: bytes):
+        cipher = Cipher(algorithms.AES(key), modes.GCM(self.iv))
+        decryptor = cipher.decryptor()
+        self.encryptedContent = decryptor.update(self.encryptedContent.encode()) + decryptor.finalize()
+        print(self.encryptedContent)
+
+    def sign(self, client):
+        message = self.content + str(self.counter).encode()
+        self.signature = client.signMessage(message)
 
     def formatChat(self):
-        chat={
+        chat = {
             "type": "signed_data",
-            "data": 
-                { 
-                    "type":self.messageType,
-                    "destination_servers": self.destinationServers,  #address of destination sevrer
-                    "iv": base64.b64encode(self.iv).decode(),   #base64 encoded IV
-                    "symm_keys":self.symmKeys,
-                    #Base64 encoded AES encrypted 
-                    "chat":base64.b64encode(self.encryptedContent).decode()
-                },
+            "data": {
+                "type": self.messageType,
+                "destination_servers": self.destinationServers,  # address of destination sevrer
+                "iv": base64.b64encode(self.iv).decode(),  # base64 encoded IV
+                "symm_keys": self.symmKeys,
+                # Base64 encoded AES encrypted
+                "chat": base64.b64encode(self.encryptedContent).decode(),
+            },
             "counter": self.counter,
-            "signature": self.signature
+            "signature": self.signature,
         }
-        return json.dumps(chat,indent=2)
-
-    
-    
+        return json.dumps(chat, indent=2)
