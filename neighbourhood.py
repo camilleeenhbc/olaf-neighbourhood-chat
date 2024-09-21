94b82903e77c94d72436e749558797e92eaf548a
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-09-21 14:35:07 +0930

    Add a neighbourhood function to send message to one server

--- neighbourhood.py
+++ neighbourhood.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import asyncio
 from websockets import WebSocketClientProtocol
 
 logging.basicConfig(level=logging.INFO)
@@ -16,39 +17,52 @@ class Neighbourhood:
     def remove_active_server(self, server_url: str):
         self.active_servers.pop(server_url)
 
+    async def send_message(self, receiver_url, message):
+        """Send message to a specific server"""
+        websocket = self.active_servers.get(receiver_url, None)
+        if websocket is None:
+            logging.error(
+                f"{self.server_url} failed to send message: Server {receiver_url} isn't active"
+            )
+            return
+
+        try:
+            await websocket.send(json.dumps(message))
+            logging.info(f"{self.server_url} broadcasted message to {receiver_url}")
+        except Exception as e:
+            logging.info(
+                f"{self.server_url} failed to send message to {receiver_url}: {e}"
+            )
+
     async def broadcast_message(self, message):
         """Broadcast the specified message to all active servers"""
-        for neighbour_url, websocket in self.active_servers.items():
-            try:
-                await websocket.send(json.dumps(message))
-                logging.info(
-                    f"{self.server_url} broadcasted message to {neighbour_url}"
-                )
-            except Exception as e:
-                logging.info(
-                    f"{self.server_url} failed to broadcast message to {neighbour_url}: {e}"
-                )
+        tasks = []
+        for neighbour_url in self.active_servers:
+            tasks.append(asyncio.create_task(self.send_message(neighbour_url, message)))
 
-    def send_client_list(self):
+        asyncio.gather(*tasks)
+
+    async def send_client_list(self):
         # send a list of all connected clients to server (requesting)
         response = {
             "type": "client_list",
             "servers": [
                 {
-                    "address": self.server_url, #server address
+                    "address": self.server_url,  # server address
                     "clients": [
                         "<Exported RSA public key of client>",
-                    ]
+                    ],
                 },
-            ]
+            ],
         }
-        pass
+        await self.broadcast_message(response)
 
-    def send_client_update(self):
+    async def send_client_update(self):
+        """Send client update when a client sends `hello` or disconnects"""
         response = {
             "type": "client_update",
             "clients": [
                 "<Exported RSA public key of client>",
-            ]
+            ],
         }
-        pass
+        await self.broadcast_message(response)
