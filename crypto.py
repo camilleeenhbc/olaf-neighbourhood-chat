542b14add3fbf7feaccedceea18148d6a913b686
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-09-28 15:19:56 +0930

    Fix crypto utility file

--- crypto.py
+++ crypto.py
@@ -5,7 +5,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.exceptions import InvalidSignature
 
 
-def generate_fingerprint(self, public_key: types.PublicKeyTypes):
+def generate_fingerprint(public_key: rsa.RSAPublicKey):
     """Generates a fingerprint based on the public key (hash)."""
     public_bytes = public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
@@ -14,7 +14,7 @@ def generate_fingerprint(self, public_key: types.PublicKeyTypes):
     return hashlib.sha256(public_bytes).hexdigest()
 
 
-def load_pem_public_key(self, public_key_str: str):
+def load_pem_public_key(public_key_str: str):
     return serialization.load_pem_public_key(
         public_key_str.encode(), backend=default_backend()
     )
